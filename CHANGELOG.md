4c277216573f0331206a0d77f7b1767be6fd12f3
Sybren A. Stüvel <sybren@stuvel.eu>
2017-10-11 13:49:15 +0200

    Initial checkin of skeleton code

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+# Flamenco Sync Client Changelog
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	stdlog "log"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const applicationVersion = "1.0-dev"
+const applicationName = "Flamenco Sync Client"
+
+var cliArgs struct {
+	version bool
+	verbose bool
+	debug   bool
+}
+
+func parseCliArgs() {
+	flag.BoolVar(&cliArgs.version, "version", false, "Shows the application version, then exits.")
+	flag.BoolVar(&cliArgs.verbose, "verbose", false, "Enable info-level logging.")
+	flag.BoolVar(&cliArgs.debug, "debug", false, "Enable debug-level logging.")
+	flag.Parse()
+}
+
+func configLogging() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	// Only log the warning severity or above by default.
+	level := log.WarnLevel
+	if cliArgs.debug {
+		level = log.DebugLevel
+	} else if cliArgs.verbose {
+		level = log.InfoLevel
+	}
+	log.SetLevel(level)
+	stdlog.SetOutput(log.StandardLogger().Writer())
+}
+
+func logStartup() {
+	level := log.GetLevel()
+	defer log.SetLevel(level)
+
+	log.SetLevel(log.InfoLevel)
+	log.WithFields(log.Fields{
+		"version": applicationVersion,
+	}).Infof("Starting %s", applicationName)
+}
+
+func main() {
+	parseCliArgs()
+	if cliArgs.version {
+		fmt.Println(applicationVersion)
+		return
+	}
+
+	configLogging()
+	logStartup()
+
+	// Set some more or less sensible limits & timeouts.
+	http.DefaultTransport = &http.Transport{
+		MaxIdleConns:          100,
+		TLSHandshakeTimeout:   30 * time.Second,
+		IdleConnTimeout:       15 * time.Minute,
+		ResponseHeaderTimeout: 30 * time.Second,
+	}
+
+}
